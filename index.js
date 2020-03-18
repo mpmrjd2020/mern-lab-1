e6859da92226fdfc52b968263337a71423da68be
Patrick Mutamba <mutambamilambop@johndeere.com>
2020-03-18 13:22:05 -0500

    Added App logic to access backend data with axios

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express()
 const usersController = require("./backend/controllers/usersControls")
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/users', usersController)
