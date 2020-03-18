6baed85e44c97db40f40a26dabb797c79dd12217
Patrick Mutamba <mutambamilambop@johndeere.com>
2020-03-18 12:39:57 -0500

    added put and delete router

--- backend/controllers/usersControls.js
+++ backend/controllers/usersControls.js
@@ -6,16 +6,28 @@ router.get('/', (req, res) => {
     User.find({}).then(users => res.json(users))
   })
 
-// router.get('/:_id', (req, res) => {
-//     console.log(req.params)
-//     User.findById(req.params._id)
-//     .then(user => res.json(user))
-// })
+router.get('/:_id', (req, res) => {
+    console.log(req.params)
+    User.findById(req.params._id)
+    .then(user => res.json(user))
+})
 
-// router.post('/', (req, res) => {
-//     console.log(req.body)
-//     User.create(req.body)
-//     .then(user => res.json(user))
-// })
+router.post('/', (req, res) => {
+    console.log(req.body)
+    User.create(req.body)
+    .then(user => res.json(user))
+})
+
+router.put('/:id', (req, res) => {
+    console.log(req.params)
+    User.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(user => res.json(user))
+})
+
+router.delete('/:id', (req, res) => {
+       console.log(req.params)
+       User.findByIdAndDelete(req.params.id)
+       .then(user => res.json(user))
+})
 
 module.exports = router
